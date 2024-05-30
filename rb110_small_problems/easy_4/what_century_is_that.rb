f3219bcf95f11f4eb98f2c7c678be21db0862bc1
Caroline Wood <carolinewood72@gmail.com>
2024-05-30 12:14:41 +0100

    updated file

--- rb110_small_problems/easy_4/what_century_is_that.rb
+++ rb110_small_problems/easy_4/what_century_is_that.rb
@@ -23,24 +23,24 @@ Need to figure out:
 
 =end
 
-def century(year)
-  century = year / 100
-  p century
-  century -= 1 if year % 100 == 0
-  century.to_s + century_suffix(century)
-end
+# def century(year)
+#   century = year / 100
+#   p century
+#   century -= 1 if year % 100 == 0
+#   century.to_s + century_suffix(century)
+# end
 
-def century_suffix(century)
-  return 'th' if [11, 12, 13].include?(century % 100)
-  last_digit = century % 10
+# def century_suffix(century)
+#   return 'th' if [11, 12, 13].include?(century % 100)
+#   last_digit = century % 10
 
-  case last_digit
-  when 1 then 'st'
-  when 2 then 'nd'
-  when 3 then 'rd'
-  else 'th'
-  end
-end
+#   case last_digit
+#   when 1 then 'st'
+#   when 2 then 'nd'
+#   when 3 then 'rd'
+#   else 'th'
+#   end
+# end
 
 # p century(2000) == '20th'
 # p century(2001) == '21st'
